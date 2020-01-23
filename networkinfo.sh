374997f231cd451419d750046d0af1cb09e40bee
n0x <ide.boschma02@gmail.com>
2020-01-23 11:16:40 +0100

    Add files via upload

--- networkinfo.sh
+++ networkinfo.sh
@@ -3,12 +3,15 @@
 two() {
 touch /tmp/source
 src="/tmp/source"
-echo "ipaddress4="$IPV4"" > $src
+echo "interface="$interface"" > $src
+echo "ipaddress4="$IPV4"" >> $src
+echo "ipv4range="$IP4RANGE"" >> $src
 echo "ipaddress6="$IPV6"" >> $src
+echo "ipv6range="$IP6RANGE"" >> $src
 echo "macaddress="$MAC"" >> $src
 echo "netmask="$NETMASK"" >> $src
 echo "broadcast="$BROADCAST"" >> $src
-source $src
+
 }
 
 one() {
@@ -17,25 +20,33 @@ IPV6=$(ifconfig $interface | grep inet | awk 'FNR == 2 {print}' | awk ' {print $
 MAC=$(ifconfig $interface | grep ether | awk ' {print $2}')
 NETMASK=$(ifconfig $interface | grep inet | awk 'FNR == 1 {print}' | awk ' {print $4}')
 BROADCAST=$(ifconfig $interface | grep inet | awk 'FNR == 1 {print}' | awk ' {print $6}')
+IP4RANGE=$(ip -4 a show $interface | grep inet | awk ' {print $2}' | tail -c 4)
+IP6RANGE=$(ip -6 a show $interface | grep inet6 | awk ' {print $2}' | tail -c 4)
 
 echo
 echo "Selected interface: $interface"
 echo
-echo "IPv4 Address: $IPV4"
-echo "IPv6 Address: $IPV6"
+echo "Interface:    $interface"
 echo "MAC Address:  $MAC"
+echo "IPv4 Address: $IPV4"
+echo "IPv4 Range:   $IP4RANGE"
+echo "IPv6 Address: $IPV6"
+echo "IPv6 Range:   $IP6RANGE"
 echo "Netmask:      $NETMASK"
 echo "Broadcast:    $BROADCAST"
 echo
 echo "Exported as:"
 echo
-echo "\$ipaddress4  = $IPV4"
-echo "\$ipaddress6  = $IPV6"
+echo "\$interface   = $interface"
 echo "\$macaddres   = $MAC"
+echo "\$ipaddress4  = $IPV4"
+echo "\$ipv4range   = $IP4RANGE"
+echo "\$ipaddress6  = $IPV6"
+echo "\$ipv6range   = $IP6RANGE"
 echo "\$netmask     = $NETMASK"
 echo "\$broadcast   = $BROADCAST"
 echo
-
+echo "To use the variables, use the command \"source /tmp/source\""
 two
 
 exit 0
@@ -55,7 +66,7 @@ echo
 
 zero() {
 banner
-read -p "manually enter interface (1) let me choose (2): " opt1
+read -p "Manually enter interface (1) Fetch interface from route (2): " opt1
 if [ "$opt1" = "1" ]; then
 read -p "enter interface: " interface
 fi
